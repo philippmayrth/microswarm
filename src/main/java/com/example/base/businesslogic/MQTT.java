0995070ed3449139c6e5a46c260c3d10cde717b8
Philipp Mayr <philipp.mayr@hs-augsburg.de>
2025-10-17 01:08:12 +0200

    Read MQTT config from config file

---
+++ src/main/java/com/example/base/businesslogic/MQTT.java
@@ -0,0 +1,77 @@
+package com.example.base.businesslogic;
+
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+
+public class MQTT {
+    private String brokerUrl;
+    private MqttClient client;
+    private String username;
+    private String password;
+
+    public MQTT(String brokerUrl, String username, String password) {
+        this.brokerUrl = brokerUrl;
+        this.username = username;
+        this.password = password;
+    }
+    
+    public MQTT(String configFilePath) {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode config = mapper.readTree(new File(configFilePath));
+            String server = config.get("server").asText();
+            int port = config.get("port").asInt();
+            this.brokerUrl = "tcp://" + server + ":" + port;
+            this.username = config.get("user").asText();
+            this.password = config.get("password").asText();
+        } catch (IOException e) {
+            System.err.println("ERROR: CONFIG FILE INVALID OR MISSING" + configFilePath);
+            e.printStackTrace();
+        }
+    }
+
+    public void connect() {
+        try {
+            client = new MqttClient(brokerUrl, MqttClient.generateClientId());
+            MqttConnectOptions options = new MqttConnectOptions();
+            options.setCleanSession(true);
+            options.setAutomaticReconnect(true);
+            options.setUserName(username);
+            options.setPassword(password.toCharArray());
+            client.connect(options);
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void disconnect() {
+        try {
+            if (client != null && client.isConnected()) {
+                client.disconnect();
+                client.close();
+            }
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void publish(String topic, String message) {
+        try {
+            if (client != null && client.isConnected()) {
+                MqttMessage mqttMessage = new MqttMessage(message.getBytes());
+                mqttMessage.setQos(1);
+                client.publish(topic, mqttMessage);
+            }
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/com/example/graphql/datafetcher/RPC.java
+++ src/main/java/com/example/graphql/datafetcher/RPC.java
@@ -1,5 +1,6 @@
 package com.example.graphql.datafetcher;
 
+import com.example.base.businesslogic.MQTT;
 import com.netflix.graphql.dgs.DgsComponent;
 import com.netflix.graphql.dgs.DgsMutation;
 import com.netflix.graphql.dgs.InputArgument;
@@ -8,11 +9,17 @@ import com.netflix.graphql.dgs.InputArgument;
 public class RPC {
 
     public RPC() {
+        // read config from mqtt.json
     }
+    
     
     @DgsMutation
     public String sendMQTTMessage(@InputArgument String topic, @InputArgument String payload) {
-        // return mqttService.publishMessage(topic, payload);
+        var mqtt = new MQTT("src/main/resources/mqtt.json");
+        mqtt.connect();
+        mqtt.publish(topic, payload);
+        mqtt.disconnect();
+
         return "Message sent to topic: " + topic + " with payload: " + payload;
     }
 
