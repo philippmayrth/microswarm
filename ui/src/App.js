4a7f4ea9c9f29020fa89de596e238e3503b62b67
Philipp Mayr <philipp.mayr2@tha.de>
2025-10-19 16:11:37 +0200

    Add react router

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,9 +1,51 @@
-import DeviceList from './components/DeviceList';
+import React from 'react'
+import {
+  createHashRouter,
+  RouterProvider,
+  Link,
+} from "react-router-dom"
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+import MainPage from './pages/MainPage'
+import ErrorPage from './pages/ErrorPage'
+
+
+// Hack to get react-router working wit MUI https://mui.com/material-ui/guides/routing/
+const LinkBehavior = React.forwardRef((props, ref) => {
+  const { href, ...other } = props;
+  // Map href (MUI) -> to (react-router)
+  return <Link ref={ref} to={href} {...other} />
+});
+
+const theme = createTheme({
+  components: {
+    MuiLink: {
+      defaultProps: {
+        component: LinkBehavior,
+      },
+    },
+    MuiButtonBase: {
+      defaultProps: {
+        LinkComponent: LinkBehavior,
+      },
+    },
+  },
+})
+
+
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <><MainPage /></>,
+    errorElement: <ErrorPage />,
+  },
+])
 
 function App() {
   return (
     <div className="App">
-      <DeviceList />
+      <ThemeProvider theme={theme}>
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </div>
   );
 }
---
+++ ui/src/pages/MainPage.js
@@ -0,0 +1,11 @@
+import DeviceList from '../components/DeviceList'
+
+function MainPage() {
+  return (
+    <>
+      <DeviceList />
+    </>
+  )
+}
+
+export default MainPage
